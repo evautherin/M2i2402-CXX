1c20671d99552affbe08407dc7ee6c8fac15130b
Etienne Vautherin <etienne@vautherin.com>
2024-02-22 19:06:07 +0100

    Initial Commit

---
+++ CXX/CXXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CXXApp.swift
+//  CXX
+//
+//  Created by Etienne Vautherin on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CXXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
